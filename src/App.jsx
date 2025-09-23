a5f7ef9cd264c487f4008354f1c6f8b4d90bf4a0
bibirama20 <ayamungkep10@gmail.com>
2025-09-23 11:09:43 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1 className="bg-red-200">Vite + React...</h1>
+      <h3>Habibi Ramadhani</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
