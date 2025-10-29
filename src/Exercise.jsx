7294f16a958fb293b2e2349661b1484fb6f9fef1
bibirama20 <ayamungkep10@gmail.com>
2025-10-29 16:14:29 +0700

    menggunakan data dummy user card

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,35 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-          name = "Habibi"
-          email = "habibi@gmail.com"
-          street = "Jl. Pagi Hari"
-          city = "Pemalang"
-          />
-           <UserCard 
-           name = "Ramadhani"
-           email = "ramadhani@gmail.com"
-           street = "Jl. Bunga Hati"
-           city = "Yogyakarta"
-           />
-            <UserCard 
-            name = "Sagara" 
-            email = "sagara@gmail.com"
-            street = "Jl. Selamat Menang"
-            city = "Semarang"
-            />
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Cards
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {users.length > 0 ? (
+          users.map((user) => <UserCard key={user.email} {...user} />)
+        ) : (
+          <p className="text-center text-gray-500 col-span-full">
+            Memuat data pengguna...
+          </p>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
