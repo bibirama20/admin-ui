a1f864cfb57a93a39baed17df2af8d4d5584cb3e
bibirama20 <ayamungkep10@gmail.com>
2025-11-04 11:39:46 +0700

    menambahkan postcads

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -19,11 +19,11 @@ function Exercise() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-        User Cards
+        Post Cards
       </h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
         {users.length > 0 ? (
-          users.map((user) => <UserCard key={user.email} {...user} />)
+          users.map((user) => <UserCard key={user.id} {...user} />)
         ) : (
           <p className="text-center text-gray-500 col-span-full">
             Memuat data pengguna...
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,27 +1,30 @@
 import React, {useState} from 'react'
 
 function UserCard(props) {
-   const{name, email, street, city,...rest} = props;
+   const{id, userId,title,body, ...rest} = props;
    const [clicked, setClicked] = useState(false);
 
    console.log(Object.entries(rest));
 
     return (
-    <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
-      <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
+    <div className="bg-white p-6 rounded-lg shadow hover:bg-Special-Red2 border shadow-md transition-shadow ">
+
       <p className="text-gray-600">
-        <span className="font-medium">Email:</span> {email}
+        <span className="font-medium "></span> {id}
       </p>
       <p className="text-gray-600">
-        <span className="font-medium">Address:</span> {street}, {city}
+        <span className="font-medium"></span> {title}
       </p>
-            {/* Menampilkan data tambahan dari rest */}
+      <p className="text-gray-600">
+        <span className="font-medium"></span> {body}
+      </p>
+        {}
       {Object.entries(rest).map(([key, value]) => (
         <p key={key} className="text-gray-600">
           <span className="font-medium capitalize">{key}:</span> {value}
         </p>
       ))}
-      <button className={`${clicked ? "bg-special-green" : "bg-gray-01"} text-white p-2 rounded-md`}
+      <button className={`${clicked ? "bg-Special-Red2" : "bg-red-800"} text-white p-2 rounded-md`}
       onClick={() => setClicked(true)}
       >
         {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
